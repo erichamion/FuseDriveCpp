6b4684d9988cfc56a7ab047b877d7d24aaaca1c2
erichamion <erichamion@gmail.com>
2015-10-13 23:15:59 -0500

    Initial commit

---
+++ fuse-drive.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   FuseDrive.cpp
+ * Author: me
+ *
+ * Created on October 13, 2015, 11:10 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
